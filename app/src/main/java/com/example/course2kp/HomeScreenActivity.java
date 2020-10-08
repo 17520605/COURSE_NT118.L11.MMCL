c57078de175e8b5a5e3282430531d31138996155
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2020-10-08 19:02:22 +0700

    Update 8/10/2020
    Update Minh Khai 08 / 10 /2020

---
+++ app/src/main/java/com/example/course2kp/HomeScreenActivity.java
@@ -0,0 +1,11 @@
+package com.example.course2kp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.course2kp.R;
+public class HomeScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
